e5a74e4e961222e2ebe9ce45fa4dc36db8bdf8d4
senasoon <senasoon0@gmail.com>
2023-01-27 00:38:40 +0900

    20230126 다른사람의 풀이

--- 프로그래머스/lv1/가장가까운글자.js
+++ 프로그래머스/lv1/가장가까운글자.js
@@ -1,19 +1,9 @@
 function solution(s) {
-  let stringArr = s.split("");
-  let answer = [];
-  for (let i = 0; i < s.length; i++) {
-    if (stringArr.slice(0, i + 1).indexOf(stringArr[i]) === i) {
-      answer.push(-1);
-    } else {
-      let frontNum = 0;
-      for (let j = i - 1; j >= 0; j--) {
-        frontNum++;
-        if (stringArr[j] === stringArr[i]) {
-          break;
-        }
-      }
-      answer.push(frontNum);
-    }
-  }
-  return answer;
+  const hash = {};
+
+  return [...s].map((v, i) => {
+    let result = hash[v] !== undefined ? i - hash[v] : -1;
+    hash[v] = i;
+    return result;
+  });
 }
