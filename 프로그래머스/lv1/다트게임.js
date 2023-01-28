517e7500c61535299b63897c77582ae6c45d9c71
seeunpark <senasoon0@gmail.com>
2023-01-28 19:16:22 +0900

    20230128 다른 사람의 풀이

--- 프로그래머스/lv1/다트게임.js
+++ 프로그래머스/lv1/다트게임.js
@@ -1,31 +1,17 @@
 function solution(dartResult) {
-  let answer = [];
-  let num = 0;
-  for (let i = 0; i < dartResult.length; i++) {
-    if (+dartResult[i] >= 0 && +dartResult[i] <= 9) {
-      if (+dartResult[i] === 1 && +dartResult[i + 1] === 0) {
-        num = 10;
-        i++;
-      } else {
-        num = +dartResult[i];
-      }
-    }
-    if (dartResult[i] === "S") answer.push(num);
-    if (dartResult[i] === "D") {
-      num = Math.pow(num, 2);
-      answer.push(num);
-    }
-    if (dartResult[i] === "T") {
-      num = Math.pow(num, 3);
-      answer.push(num);
-    }
-    if (dartResult[i] === "*") {
-      answer[answer.length - 1] *= 2;
-      answer[answer.length - 2] *= 2;
-    }
-    if (dartResult[i] === "#") {
-      answer[answer.length - 1] = -answer[answer.length - 1];
-    }
+  const bonus = { S: 1, D: 2, T: 3 },
+    options = { "*": 2, "#": -1, undefined: 1 };
+
+  let darts = dartResult.match(/\d.?\D/g);
+
+  for (let i = 0; i < darts.length; i++) {
+    let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/),
+      score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+    if (split[3] === "*" && darts[i - 1]) darts[i - 1] *= options["*"];
+
+    darts[i] = score;
   }
-  return answer.reduce((acc, cur) => acc + cur, 0);
+
+  return darts.reduce((a, b) => a + b);
 }
