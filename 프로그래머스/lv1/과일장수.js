1f9efd2704d4c0631c2f2b8c4625aa8d5d160531
senasoon <senasoon0@gmail.com>
2023-01-30 10:38:33 +0900

    20230130

---
+++ 프로그래머스/lv1/과일장수.js
@@ -0,0 +1,9 @@
+function solution(k, m, score) {
+  let answer = 0;
+  score.sort((a, b) => b - a);
+  for (let i = 0; i < score.length; i += m) {
+    if (i + m > score.length) break;
+    answer += Math.min(...score.slice(i, i + m)) * m;
+  }
+  return answer;
+}
