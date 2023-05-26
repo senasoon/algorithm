16c3b8592269ff3ea6f768084c04f633ac99a3e7
senasoon <senasoon0@gmail.com>
2023-05-26 20:05:09 +0900

    20230526 수정

--- 프로그래머스/lv1/신규아이디추천.js
+++ 프로그래머스/lv1/신규아이디추천.js
@@ -1,29 +1,14 @@
 function solution(new_id) {
-  new_id = new_id.toLowerCase();
-  new_id = new_id.match(/[a-z0-9\-\_\.]/g).join('');
-  new_id = new_id.replace(/\.\.+/g, '.');
-  if (new_id[0] === '.') {
-    new_id = new_id.slice(1);
-  }
-  if (new_id[new_id.length - 1] === '.') {
-    new_id = new_id.slice(0, new_id.length - 1);
-  }
-  if (new_id === '') {
-    new_id = 'a';
-  }
-  if (new_id.length >= 16) {
-    new_id = new_id.slice(0, 15);
-    if (new_id[14] === '.') {
-      new_id = new_id.slice(0, new_id.length - 1);
-    }
-  }
-  if (new_id.length <= 2) {
-    const last_str = new_id[new_id.length - 1];
-    for (let i = new_id.length; i < 3; i++) {
-      new_id += last_str;
-    }
-  }
-  return new_id;
+  const answer = new_id
+    .toLowerCase()
+    .replace(/[^\w-_.]/g, '')
+    .replace(/\.+/g, '.')
+    .replace(/^\.|\.$/g, '')
+    .replace(/^$/, 'a')
+    .slice(0, 15)
+    .replace(/\.$/, '');
+
+  return answer.padEnd(3, answer[answer.length - 1]);
 }
 
 console.log(solution('...!@BaT#*..y.abcdefghijklm'));
